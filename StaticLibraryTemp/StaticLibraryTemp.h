03bfcdbce8f2fcaee433e390c2fb06e892e65fa2
xiaoanning <734871221@qq.com>
2017-07-18 10:34:51 +0800

    Initial Commit

---
+++ StaticLibraryTemp/StaticLibraryTemp.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibraryTemp.h
+//  StaticLibraryTemp
+//
+//  Created by 安宁 on 2017/7/18.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibraryTemp : NSObject
+
+@end
---
+++ StaticLibraryTemp/StaticLibraryTemp.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibraryTemp.m
+//  StaticLibraryTemp
+//
+//  Created by 安宁 on 2017/7/18.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import "StaticLibraryTemp.h"
+
+@implementation StaticLibraryTemp
+
+@end
